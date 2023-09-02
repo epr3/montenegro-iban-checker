f49306074b5f07349ec7071545fb042023763297
Eduard Predescu <eduard.florin.predescu@gmail.com>
2023-09-02 21:18:13 +0300

    added unocss

--- iban-checker-api/main.py
+++ iban-checker-api/main.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -9,7 +7,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/check-iban")
+def check_iban(iban: str):
+    return iban
